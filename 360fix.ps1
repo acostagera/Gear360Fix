ea941fd97fc84484bd032f63c1b0c244cf33a51f
acostagera <acostagera@gmail.com>
2018-04-23 14:17:20 -0700

    Add files via upload

---
+++ 360fix.ps1
@@ -0,0 +1,6 @@
+﻿$oldvids = Get-ChildItem .\*XHC.MP4 -Recurse
+
+foreach ($oldvid in $oldvids) {
+    $fixedvid = $oldvid.BaseName+"_Fixed.MP4"
+    .\ffmpeg.exe -i $oldvid.Name -y -c:v copy -c:a copy $fixedvid
+    }
